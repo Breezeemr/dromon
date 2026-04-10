b7294a260691098a9e3455159f10e5f21f792b6e
Tyler Tallman <dspiteself@gmail.com>
2026-04-09 22:00:06 -0500

    delete scaffold test

--- fhir-defintions-to-malli/test/com/breezeehr/fhir_defintions_to_malli_test.clj
+++
@@ -1,7 +0,0 @@
-(ns com.breezeehr.fhir-defintions-to-malli-test
-  (:require [clojure.test :refer :all]
-            [com.breezeehr.fhir-defintions-to-malli :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
